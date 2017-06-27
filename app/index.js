905f426583358aa4b906bbbbcaf5d213e9b4731b
Sunim <sunim.54@gmail.com>
2017-06-27 22:21:25 +0545

    Return div text

--- app/index.js
+++ app/index.js
@@ -2,3 +2,10 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
+class App extends React.Component{
+    render(){
+        return(
+            <div>Yo Man!</div>
+        )
+    }
+}
