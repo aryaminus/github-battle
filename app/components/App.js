db6ec921457827b8f8b2e7c596b1feac4c694df0
Sunim <sunim.54@gmail.com>
2017-06-30 17:32:57 +0545

    Add pupular component to render it from app

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,14 @@
 var React = require('react');
+var Popular = require('./Popular');
 
 class App extends React.Component{
     render(){
         return(
             <div>
-                Yo Man!
+                <Popular />
             </div>
         )
     }
 }
+
+module.exports = App;//needed for validity of require in index
---
+++ app/components/Popular.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Popular extends React.Component{
+    render(){
+        return(
+            <div>
+                Popular Man!
+            </div>
+        )
+    }
+}
+
+module.exports = Popular;
