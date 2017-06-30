428b3b98416272da348e2385baa2cc4ea5d3e67b
Sunim <sunim.54@gmail.com>
2017-06-30 20:57:27 +0545

    List making

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,10 +2,17 @@ var React = require('react');
 
 class Popular extends React.Component{
     render(){
+        var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']; 
         return(
-            <div>
-                Popular Man!
-            </div>
+            <ul>
+                {languages.map(function(lang){
+                    return(
+                        <li>
+                            {lang}
+                        </li>
+                    )
+                })}
+            </ul>
         )
     }
 }
